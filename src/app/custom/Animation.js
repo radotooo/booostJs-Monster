8aa832c07b0339180dd1d2e10b90722460d720fd
radotooo <radotooo@gmail.com>
2021-01-28 11:58:25 +0200

    fix

--- src/app/custom/Animation.js
+++ src/app/custom/Animation.js
@@ -1,8 +1,25 @@
+import EventEmitter from 'eventemitter3';
 import Monster from './Monster';
 
 export default class Animation {
   constructor() {
     this.monsters = [];
+  }
+  mouseEnterHandler(el) {
+    el.element.id = 'expand';
+    this.monsters.map((x) =>
+      x.element.id === 'expand' ? x.expand() : x.contract()
+    );
+  }
+
+  mouseLeaveHandler(el) {
+    el.element.id = 'contract';
+    this.monsters.map((x) => x.contract());
+  }
+
+  containerHandler() {
+    this.monsters.map((x) => (x.element.id = 'reset'));
+    this.monsters.map((x) => x.reset());
   }
 
   init() {
@@ -11,16 +28,33 @@ export default class Animation {
     monsters.forEach((x) => {
       const monster = new Monster(x);
 
-      monster.element.addEventListener('mouseenter', (el) =>
-        monster.expand(el)
-      );
-      monster.element.addEventListener('mouseleave', (el) =>
-        monster.contract(el)
-      );
-      monster.element.parentElement.addEventListener('mouseleave', (el) =>
-        monster.reset(el)
-      );
       this.monsters.push(monster);
     });
+
+    this.monsters.map((x) =>
+      x.element.addEventListener('mouseenter', () => this.mouseEnterHandler(x))
+    );
+
+    this.monsters.map((x) =>
+      x.element.addEventListener('mouseleave', () => this.mouseLeaveHandler(x))
+    );
+    const container = document.querySelector('#container');
+
+    container.addEventListener('mouseleave', (element) =>
+      this.containerHandler(element)
+    );
+
+    //     this.mouseEnterHandler(el)
+    //   );));
+    // monster.element.id = 'reset';
+    //   monster.element.addEventListener('mouseenter', (el) =>
+    //     this.mouseEnterHandler(el)
+    //   );
+    //   monster.element.addEventListener('mouseleave', (el) =>
+    //     monster.contract(el)
+    //   );
+    //   monster.element.parentElement.addEventListener('mouseleave', (el) =>
+    //     monster.reset(el)
+    //   );
   }
 }
